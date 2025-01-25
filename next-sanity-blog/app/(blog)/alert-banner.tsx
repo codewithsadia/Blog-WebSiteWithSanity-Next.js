0e31ca9bd67150ed2377a80c66039fbfd8d85da6
sadiaimran <sadiaimranmalik24@gmail.com>
2025-01-25 22:45:01 +0500

    done

---
+++ next-sanity-blog/app/(blog)/alert-banner.tsx
@@ -0,0 +1,11 @@
+
+export default function AlertBanner() {
+  return (
+    <div
+      className="fixed top-0 left-0 z-50 w-full border-b bg-white/95 text-black backdrop-blur">
+      <div className="py-2 text-center text-sm">
+        Blogs
+      </div>
+    </div>
+  );
+}
---
+++ next-sanity-blog/sanity/lib/demo.ts
@@ -0,0 +1,42 @@
+/**
+ * Demo data used as placeholders and initial values for the blog
+ */
+
+export const title = "Forex Blog.";
+
+export const description = [
+  {
+    _key: "9f1a629887fd",
+    _type: "block",
+    children: [
+      {
+        _key: "4a58edd077880",
+        _type: "span",
+        marks: [],
+        text: "Welcome to Blog WebSite ",
+      },
+//      {
+  //      _key: "4a58edd077882",
+    //    _type: "span",
+      //  marks: [],
+        //text: " and ",
+      //},
+      //{
+        //_key: "4a58edd077883",
+        //_type: "span",
+        //marks: ["1f8991913ea8"],
+        //text: "Sanity",
+      //},
+      //{
+        //_key: "4a58edd077884",
+        //_type: "span",
+        //marks: [],
+        //text: ".",
+      //},
+    ],
+    
+    style: "normal",
+  },
+];
+
+export const ogImageTitle = "A Next.js Blog with a Native Authoring Experience";
